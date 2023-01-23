f3ca97bd91e41158147f0f5abdb1582f516d5fa1
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-23 11:31:24 +0300

    created YourBotArmy component

--- src/Components/BotCard.js
+++ src/Components/BotCard.js
@@ -2,4 +2,4 @@ import React from "react";
 
 function BotCard(){}
 
-export default BotCard
+export default BotCard;
---
+++ src/Components/YourBotArmy.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const YourBotArmy = function(){}
+
+export default YourBotArmy;
