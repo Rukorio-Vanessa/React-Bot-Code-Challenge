8e6f6a062dc322038cf04f8ed73905ba05bf1d1c
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-23 18:20:37 +0300

    created a card to hold bots

--- src/Components/BotCard.js
+++ src/Components/BotCard.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-function BotCard(){}
+
+function BotCard({bot}){
+    return(
+      <div className="col-3 p-1 my-2">
+      <div className="card h-100" style={{width:"18rem"}}>
+          <img src="#" className="card-img-top " alt={"bot pic"}/>
+          <div className="card-body">
+              <h5 className="card-title">{"Bot Name"}</h5>
+              <p className="card-text">{"catchphrase"}</p>
+          </div>
+      </div>
+  </div>
+    )
+}
 
 export default BotCard;
