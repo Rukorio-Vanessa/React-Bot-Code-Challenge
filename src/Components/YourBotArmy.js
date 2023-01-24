fa8d428b823291b93071cf0db7d5d24ce6253996
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-24 09:01:57 +0300

    defined YourBotArmy componenent

--- src/Components/YourBotArmy.js
+++ src/Components/YourBotArmy.js
@@ -1,5 +1,30 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-const YourBotArmy = function(){}
+const YourBotArmy = function({bots, deleteBot, removeBot}){
+    const selectedBotArmy =  bots.map((bot)=>{
+        return (
+            <CardBot
+            key={bot.id}
+            bot={bot}
+            clickEvent={removeBot}
+            deleteBot={deleteBot}
+            />
+        )
+    })
+
+
+    return(
+        <div className= 'd-block p-2 bg-secondary army'>
+            <strong className="text-light">BOT BATTLR:click to add bot to your army!</strong>
+            <div className="container mt-5">
+                <div className="row army-row">
+                    {selectedBotArmy}
+                </div>
+            </div>
+        </div>
+    )
+}
+
 
 export default YourBotArmy;
